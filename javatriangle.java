508649de6adf34385c746dd907c23173004430d2
Devika-Gopan <73544738+Devika-Gopan@users.noreply.github.com>
2021-01-10 23:43:53 +0530

    Add files via upload

---
+++ javatriangle.java
@@ -0,0 +1,15 @@
+class Triangle
+{
+	Triangle()
+	{
+		System.out.println("Perimeter = "+(3+4+5)+"units");
+		System.out.println("Area      = "+(.5*3*4)+"sq.units");
+	}
+}
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Triangle obj = new Triangle();
+	}
+}
