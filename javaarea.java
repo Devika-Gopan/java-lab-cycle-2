19e03687e00fc2aa113bce52a12e226bc63da484
Devika-Gopan <73544738+Devika-Gopan@users.noreply.github.com>
2021-01-10 21:43:45 +0530

    Update javaarea.java

--- javaarea.java
+++ javaarea.java
@@ -2,18 +2,18 @@ public class main
 {
 	public static void main(String args[])
 	{
-		Testclass obj=new Testclass();
+		Area obj=new Area();
 		obj.Area( 4);
 		obj.Area(5,4);
 	}
 }
-class Testclass
+class Area
 {
-	void Area(int r)
+	void Area(double r)
 	{
 		System.out.println("Area of circle    = "+ (3.14*r*r)+"sq.units");
 	}
-	void Area(int l,int b)
+	void Area(double l,double b)
 	{
 		System.out.println("Area of rectangle = "+(l*b)+"sq.units");
 	}
